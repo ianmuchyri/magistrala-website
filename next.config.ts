963ec94851f214eccc6524e5629d9ccc427bcae7
Nataly Musilah <115026536+Musilah@users.noreply.github.com>
2025-01-16 14:51:41 +0300

    NOISSUE - Fix deployment issue (#3)
    Signed-off-by: Musilah <nataleigh.nk@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  assetPrefix: '/mg-website/',
-  basePath: '/mg-website',
+  assetPrefix: isProd ? '/mg-website/' : '',
+  basePath: isProd ? '/mg-website' : '',
   images: {
     unoptimized: true,
   },
